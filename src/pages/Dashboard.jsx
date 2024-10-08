177a33d67b8208592c9ca1eb92cd83a220673d3f
miiaa <miia.anttila@aalto.fi>
2024-10-08 18:27:31 +0300

    Dashboard done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -18,8 +18,8 @@ const Dashboard = ({ appLogo }) => {
   }
 
   const handleLogout = () => {
-      // Add your logout logic here (e.g., clear session or tokens)
-      navigate('/login'); // Redirect back to login page after logging out
+      // Add logic later
+      navigate('/login') 
   }
 
 
@@ -49,12 +49,41 @@ const Dashboard = ({ appLogo }) => {
       </div>
 
       <div className="course-section">
-          <p className="course-info">
-              Selected Course: <strong>{selectedCourse || 'No course selected'}</strong>
-          </p>
+          <p>Selected course:</p>
+          <h3 className="course-info">
+              <strong>{selectedCourse || 'No course selected'}</strong>
+          </h3>
           <button className="change-course-button" onClick={handleCourseChange}>
              Change Course
           </button>
+      </div>
+
+      <div className="content-container">
+          <div className="info-section">
+              <h3 className="teammatesfinding-header">
+                Teammates Finding
+              </h3> 
+              <p className="teammatesfinding-info">
+                View your project's applications or find your teammates by searching existing projects.
+              </p>
+              <a href="/teammatesFinding">Teammates Finding</a>
+
+              <h3 className="projectproposal-header">
+                Project Proposal
+              </h3> 
+              <p className="projectproposal-info">
+                Have an idea for project? Create a new project, where you can add students and students can apply to your project. 
+              </p>
+              <a href="/projectProposal">Project Proposal</a>
+
+              <h3 className="projectfinding-header">
+                Project Finding
+              </h3> 
+              <p className="projectfinding-info">
+                Search existing projects and apply them.
+              </p>
+              <a href="/projectFinding">Project Finding</a>
+          </div>
       </div>
     </>
   )
